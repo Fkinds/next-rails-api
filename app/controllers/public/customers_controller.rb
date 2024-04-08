faa0f47c1d9942ca6450c9bf2a56974745cdabe3
Fkinds <Fkinds1020@gmail.com>
2024-04-08 17:27:10 +0900

    create session action

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,13 @@
 class Public::CustomersController < ApplicationController
+  def session
+    uid = request.headers['uid']
+    if uid 
+      data = "aaaa"
+      render json: data
+    else
+      render status: 1
+    end 
+    # @customer = Customer.find_by_uid(uid)
+    # render status: 200, json: @customer
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,26 @@
 Rails.application.routes.draw do
   scope module: :public do 
     mount_devise_token_auth_for 'Customer', at: 'auth', controllers:{
-    registrations: 'registrations',
-    sessions: 'sessions',
+    registrations: 'public/registrations',
+    sessions: 'public/sessions',
   }
   end
 
   scope module: :public do
     root to: "homes#top"
-    get "about" => "homes#about", as: "about"
-    get "customers/my_page" => "customers#show", as: "my_page"
-    get "customers/information/edit" => "customers#edit"
-    get "customers/confirm" => "customers#confirm"
-    post "customers/withraw" => "customers#withdraw"
-    patch "customers/information" => "customers#update"
-    delete "cart_items/destroy_all" => "cart_items#destroy_all"
-    resources :cart_items, except: [:show, :new, :edit]
-    resources :items, only: [:index, :show]
-    post "orders/confirm" => "orders#confirm"
-    get "orders/complete" => "orders#complete"
-    resources :orders, execpt: [:update, :destroy, :edit]
-    resources :addresses, except: [:show, :new]
+    get "customers/session" => "customers#session"
+  #   get "about" => "homes#about", as: "about"
+  #   get "customers/my_page" => "customers#show", as: "my_page"
+  #   get "customers/information/edit" => "customers#edit"
+  #   get "customers/confirm" => "customers#confirm"
+  #   post "customers/withraw" => "customers#withdraw"
+  #   patch "customers/information" => "customers#update"
+  #   delete "cart_items/destroy_all" => "cart_items#destroy_all"
+  #   resources :cart_items, except: [:show, :new, :edit]
+  #   resources :items, only: [:index, :show]
+  #   post "orders/confirm" => "orders#confirm"
+  #   get "orders/complete" => "orders#complete"
+  #   resources :orders, execpt: [:update, :destroy, :edit]
+  #   resources :addresses, except: [:show, :new]
   end
 end
